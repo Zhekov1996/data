ea04692c1da5555e78d244e3fbfdbb038523e723
Nikolay Zhekov <n.jekov96@abv.bg>
2022-02-03 16:01:14 +0200

    Initial commit

---
+++ src/test/java/operators/java/EqualOperations.java
@@ -0,0 +1,18 @@
+package operators.java;
+
+public class EqualOperations {
+    public static void main(String[] args) {
+        int firstNumber = 5;
+        int secondNumber = 2;
+
+        //equals
+        System.out.println(firstNumber == secondNumber);
+
+        //not equals
+        System.out.println(firstNumber != secondNumber);
+
+        //bigger or equal
+        System.out.println(firstNumber >= secondNumber);
+
+    }
+}
